eda484db11dec0c188c04a0406bf831d11f157f4
Yolijn <yolijn@frameless.io>
2023-04-04 14:03:57 +0200

    add fonts

---
+++ src/theme/Root.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import '@nl-design-system-unstable/nlds-design-tokens/src/font'
+
+// Default implementation, that you can customize
+export default function Root ({ children }) {
+  return <>{children}</>
+}
