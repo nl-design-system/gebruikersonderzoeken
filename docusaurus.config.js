8e8071ad9cac95c608fa8740f5fd248683be9a59
Robbert Broersma <git@robbertbroersma.nl>
2023-07-12 14:25:07 +0200

    build: update baseUrl to work with gebruikersonderzoeken.nl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,10 +12,11 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://gebruikersonderzoeken.nl',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/gebruikersonderzoek/',
+  // baseUrl: '/gebruikersonderzoek/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
