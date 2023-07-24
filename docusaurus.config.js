12d3dd6958cbe8f1e8bb27b0464e865dabc0ff45
Hidde de Vries <hidde@hiddedevries.nl>
2023-07-24 10:49:19 +0200

    Remove some default  docusaurus stuff

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,20 +18,14 @@ const config = {
   // baseUrl: '/gebruikersonderzoek/',
   baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'nl-design-system', // Usually your GitHub org/user name.
+  projectName: 'gebruikersonderzoek', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'nl',
+    locales: ['nl'],
   },
 
   presets: [
@@ -61,8 +55,6 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
       navbar: mainNav,
       tableOfContents: {
         minHeadingLevel: 2,
