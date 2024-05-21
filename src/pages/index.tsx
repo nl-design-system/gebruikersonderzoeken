f30bab6e84505b9d53db936d44c87dc0f58c66bc
Jeroen du Chatinier <49227194+JeroenduC@users.noreply.github.com>
2024-05-21 15:16:26 +0200

    Tagline aanpassing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ function HomepageHeader() {
       <div className="container">
         <h1 className="hero__title">Gebruikersonderzoeken</h1>
         <p>
-          Hier verzamelen en delen we zoveel mogelijk gebruikersonderzoeken van alle gemeenten in Nederland voor
+          Hier verzamelen en delen we zoveel mogelijk gebruikersonderzoeken van alle overheidsinstanties in Nederland voor
           onderzoekers, ontwerpers en managers. Zodat we zoveel mogelijk van elkaar kunnen leren, geinspireerd raken
           door elkaar's onderzoek en sneller de juiste beslissingen kunnen nemen.
         </p>
