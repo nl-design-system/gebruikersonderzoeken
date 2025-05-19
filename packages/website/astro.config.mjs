f04f414407acfa53ecd9cbf5f1d30b0fd611fa42
Peter Goes <petergoes@gmail.com>
2025-05-19 17:43:55 +0200

    chore: add react to Astro

--- packages/website/astro.config.mjs
+++ packages/website/astro.config.mjs
@@ -1,5 +1,8 @@
+import react from '@astrojs/react';
 // @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react()],
+});
