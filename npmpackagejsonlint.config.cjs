8b8a25809f529103b4e53ee8b877d57b068063c2
Peter Goes <petergoes@gmail.com>
2025-05-20 10:13:44 +0200

    chore: update package scope to be an existing scope

--- npmpackagejsonlint.config.cjs
+++ npmpackagejsonlint.config.cjs
@@ -9,7 +9,7 @@ module.exports = {
   ],
   rules: {
     'valid-values-license': ['error', ['EUPL-1.2']],
-    'valid-values-name-scope': ['error', ['@gebruikersonderzoeken']],
+    'valid-values-name-scope': ['error', ['@nl-design-system-community']],
     'no-caret-version-dependencies': 'error',
     'no-caret-version-devDependencies': 'error',
     'no-tilde-version-dependencies': 'error',
