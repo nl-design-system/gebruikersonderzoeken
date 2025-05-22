2aced1e006f776e239962c47c4211083de8ef607
Peter Goes <petergoes@gmail.com>
2025-05-22 14:23:02 +0200

    chore: point eslint and prettier to astro files as well

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -20,9 +20,17 @@ module.exports = {
         singleQuote: false,
       },
     },
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
   ],
   // Use a printWith of 120 characters which is different from the default 80
   printWidth: 120,
+  // Use the astro plugin to format .astro files
+  plugins: ['prettier-plugin-astro'],
   // Use single quotes (') instead of the default double quotes ("), but see below.
   singleQuote: true,
 };
