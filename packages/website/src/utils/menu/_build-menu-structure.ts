99f029ee36e0d3a6c005c7e99013828bdbeae8bb
Peter Goes <petergoes@gmail.com>
2025-05-26 13:47:09 +0200

    docs: add docs to explain the working of _build-menu-structure

--- packages/website/src/utils/menu/_build-menu-structure.ts
+++ packages/website/src/utils/menu/_build-menu-structure.ts
@@ -33,8 +33,17 @@ type Item = Folder | Page;
 
 const DOCS_DIR = path.join(process.cwd(), '../../docs/');
 
-const isNotIgnoredPath = (path: string) => path.split('/').reverse()?.[0].startsWith('_') === false;
+/**
+ * Ignore files starting with an underscore
+ */
+const isNotIgnoredPath = (path: string) => path.split('/').reverse()?.[0]?.startsWith('_') === false;
 
+/**
+ * Create a label from the file name. Used as a fallback for when there is no
+ * actual label available
+ * 1. The first letter is capitalised
+ * 2. dashes are converted to spaced
+ */
 const getLabelFromPath = (path: string) =>
   path
     .split('/')
@@ -42,11 +51,22 @@ const getLabelFromPath = (path: string) =>
     ?.replace(/^./, (char) => char.toUpperCase())
     ?.replace('-', ' ');
 
+/**
+ * A pathId, is the path to a file. The parent is the folder that which contains
+ * the file. The parentId is, thus, the path tot the parent folder
+ */
 const getParentId = (path: string) => path.split('/').reverse().splice(1).reverse().join('/') || undefined;
 
-const getItemsWithParent = (collection: Item[], parent?: string): Item[] =>
+/**
+ * Get all children that have a specific parentId. When no parent is specified
+ * the children with a missing parentId (root level items) are returned
+ */
+const getChildrenOfParent = (collection: Item[], parent?: string): Item[] =>
   collection.filter((item) => item.parentId === parent);
 
+/**
+ * Build a Folder object for a given path.
+ */
 async function buildFolder(path: string) {
   const category = (await readFile(`${DOCS_DIR}${path}/_category_.json`, { encoding: 'utf-8' })
     .then((content) => JSON.parse(content))
@@ -66,26 +86,41 @@ async function buildFolder(path: string) {
   return folder;
 }
 
-async function buildPage(path: string) {
+/**
+ * Build a Page object for a given path
+ */
+async function buildPage(path: string): Promise<Page> {
   const contents = await readFile(`${DOCS_DIR}${path}`, { encoding: 'utf-8' });
-
   const { frontmatter } = parseFrontmatter(contents);
 
   const id = path.replace('.md', '').replace('/README', '');
   const label = frontmatter?.['pagination_label'] ?? frontmatter?.['title'] ?? getLabelFromPath(path);
   const slug = frontmatter['slug'] ? `/docs${frontmatter?.['slug']}` : `/docs/${id}`;
 
-  const page: Page = {
+  return {
     id,
     collapsible: false,
     label,
     parentId: getParentId(path),
     slug,
   };
-
-  return page;
 }
 
+/**
+ * Get a menu structure based on the markdown files in the `docs/` directory.
+ * The structure is nested like the file structure.
+ *
+ * **`label` field**
+ * The `label` fields are filled with values from the frontmatter (markdown
+ * files) or the content of the `_category_.json` files (folders).
+ * When these values are missing, a fallback value is generated from the file path
+ *
+ * **`slug` field**
+ * The slug is taken from the frontmatter of the markdown file, when not present
+ * the file path (without the exention) is being used. Note that `/README.ext`
+ * is treated as a index route, and thus becomes `/`. Also there is a folder with
+ * a space. This is dealt with as much as possible to keep urls working
+ */
 export const getMenuStructure = () =>
   readdir(DOCS_DIR, { recursive: true })
     .then((list) => list.filter(isNotIgnoredPath))
@@ -107,12 +142,12 @@ export const getMenuStructure = () =>
       ),
     )
     .then((collection: Item[]) => {
-      const menu = getItemsWithParent(collection, undefined);
+      const menu = getChildrenOfParent(collection, undefined);
 
       function collectChildrenInItems(children: Item[]) {
         children.forEach((item) => {
           if (item.collapsible === true) {
-            item.children = getItemsWithParent(collection, item.id);
+            item.children = getChildrenOfParent(collection, item.id);
             collectChildrenInItems(item.children);
           }
         });
