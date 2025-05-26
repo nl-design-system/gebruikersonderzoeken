ffb3907b372c75861de08f27abf8931bd390884d
Peter Goes <petergoes@gmail.com>
2025-05-26 11:57:43 +0200

    refactor: build menu structure in regular code flow instead of separate script
    Before, the menu structure was build via a separate script which
    exported to a json file. That file was read and parsed during code
    execturion (build time). However, that meant having the file written to
    disk, which should be gitignored.
    
    Having it part of the regular code flow, there is no need for a
    dedicated json file.

--- packages/website/src/utils/get-menu-items.ts
+++ packages/website/src/utils/get-menu-items.ts
@@ -1,5 +1,8 @@
 import type { MenuItem } from '@nl-design-system-community/ma-components/local/menu/index.tsx';
 import { isPage, isFolder } from '@nl-design-system-community/ma-components/local/menu/index.tsx';
+import { getMenuStructure } from './_build-menu-structure.ts';
+
+const menuItems = await getMenuStructure();
 
 function isActivePage(item: MenuItem, pathName: string) {
   return isPage(item) && pathName.includes(item.slug);
@@ -19,19 +22,25 @@ function isActiveFolder(children: MenuItem[], pathName: string): boolean {
   return false;
 }
 
-export function markActiveMenuItems(items: MenuItem[], currentUrl: URL) {
-  const pathName = currentUrl.pathname;
+/**
+ * Get a data structure for the menu items. When the `currentUrl` is provided,
+ * the menu items are marked as `current`.
+ */
+export function getMenuItems(currentUrl?: URL) {
+  const pathName = currentUrl?.pathname;
 
-  const itemsClone = structuredClone(items);
+  const itemsClone = structuredClone(menuItems);
 
   function markItem(item: MenuItem) {
-    if (isActivePage(item, pathName)) {
-      item.current = 'page';
-    }
+    if (pathName) {
+      if (isActivePage(item, pathName)) {
+        item.current = 'page';
+      }
 
-    if (isFolder(item) && isActiveFolder(item.children, pathName)) {
-      item.current = true;
-      item.children.forEach(markItem);
+      if (isFolder(item) && isActiveFolder(item.children, pathName)) {
+        item.current = true;
+        item.children.forEach(markItem);
+      }
     }
   }
 
