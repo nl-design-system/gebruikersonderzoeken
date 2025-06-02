c14fce5ffb74c82b960f39c8e880e68217e11db3
Peter Goes <petergoes@gmail.com>
2025-06-02 16:19:26 +0200

    feat: add cover option to frontmatter and use it as share image

--- packages/website/src/content.config.ts
+++ packages/website/src/content.config.ts
@@ -4,6 +4,12 @@ import { glob } from 'astro/loaders'; // Not available with legacy API
 const onderzoeken = defineCollection({
   loader: glob({ base: '../../docs/', pattern: '**/*.md' }),
   schema: z.object({
+    cover: z
+      .object({
+        alt: z.string(),
+        url: z.string(),
+      })
+      .optional(),
     title: z.string(),
   }),
 });
