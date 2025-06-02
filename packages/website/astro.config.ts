0334220392331df0458a05ee535ff15117bbe4c2
Peter Goes <petergoes@gmail.com>
2025-06-02 15:46:11 +0200

    feat: add remark plugin which extracts the first image as cover in frontmatter

--- packages/website/astro.config.ts
+++ packages/website/astro.config.ts
@@ -1,9 +1,13 @@
 import react from '@astrojs/react';
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { coverPlugin } from './remark-cover-plugin.ts';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  markdown: {
+    remarkPlugins: [coverPlugin],
+  },
   site: 'https://gebruikersonderzoeken.nl',
 });
