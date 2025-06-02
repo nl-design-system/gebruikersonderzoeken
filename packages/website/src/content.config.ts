184e1db82dcd74ebf319b6a19bc4d8e3c5e822ec
Yolijn <mail@yolijn.com>
2025-06-02 19:23:23 +0200

    refactor: use zod library to create types and typeguards

--- packages/website/src/content.config.ts
+++ packages/website/src/content.config.ts
@@ -1,17 +1,20 @@
 import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders'; // Not available with legacy API
 
+export const CoverSchema = z.object({
+  alt: z.string(),
+  url: z.string(),
+});
+
+export const PageSchema = z.object({
+  cover: CoverSchema.optional(),
+  description: z.string().optional(),
+  title: z.string(),
+});
+
 const onderzoeken = defineCollection({
   loader: glob({ base: '../../docs/', pattern: '**/*.md' }),
-  schema: z.object({
-    cover: z
-      .object({
-        alt: z.string(),
-        url: z.string(),
-      })
-      .optional(),
-    title: z.string(),
-  }),
+  schema: PageSchema,
 });
 
 export const collections = { onderzoeken };
