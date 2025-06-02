a10943f2006b023bf048cdffaa788e158710767d
Peter Goes <petergoes@gmail.com>
2025-06-02 11:16:42 +0200

    feat: add opengraph meta tags

--- packages/website/src/layouts/default.tsx
+++ packages/website/src/layouts/default.tsx
@@ -18,11 +18,21 @@ export function Default(props: PropsWithChildren<Props>) {
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
         <title>{title}</title>
         {description && <meta name="description" content={description} />}
 
         <link rel="canonical" href={canonical.href} />
         <link rel="icon" type="image/svg+xml" href="/favicon.ico" />
+
+        {/* OpenGraph data */}
+        <meta property="og:url" content={canonical.href} />
+        <meta property="og:locale" content={lang} />
+        <meta property="og:title" content={title} />
+        {description && <meta property="og:description" content={description} />}
+
+        {/* X */}
+        <meta name="twitter:card" content="summary_large_image" />
       </head>
       <body>{props.children}</body>
     </html>
