910514b026dd00c6d8fec592af1c8c8bbdac6218
Peter Goes <petergoes@gmail.com>
2025-06-17 11:36:00 +0200

    chore: disable the astro toolbar
    It throws a lot of CSP warnings for things we do want to catch. And the
    benefit of the toolbar is limited.

--- packages/website/astro.config.ts
+++ packages/website/astro.config.ts
@@ -8,6 +8,9 @@ export default defineConfig({
   build: {
     inlineStylesheets: 'never',
   },
+  devToolbar: {
+    enabled: false,
+  },
   integrations: [react()],
   markdown: {
     remarkPlugins: [coverPlugin],
