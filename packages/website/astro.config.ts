84d13d23c3471890ad51d34bce54f26d6d2523cf
Peter Goes <petergoes@gmail.com>
2025-06-17 09:22:29 +0200

    chore: prevent inline stylesheets
    prevent inline styles because it violates the csp policy

--- packages/website/astro.config.ts
+++ packages/website/astro.config.ts
@@ -5,6 +5,9 @@ import { coverPlugin } from './remark-cover-plugin.ts';
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    inlineStylesheets: 'never',
+  },
   integrations: [react()],
   markdown: {
     remarkPlugins: [coverPlugin],
