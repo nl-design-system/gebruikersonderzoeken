22e945fe1b5d4643f2fd64e35ba127fb85eb6109
Peter Goes <petergoes@gmail.com>
2025-06-20 13:30:38 +0200

    refactor: panel includes design tokens in css instead of markup
    this way, the postcss plugin knows about the used design tokens

--- packages/ma-components/local/panel/index.tsx
+++ packages/ma-components/local/panel/index.tsx
@@ -1,4 +1,4 @@
-import type { JSX, CSSProperties, PropsWithChildren } from 'react';
+import type { JSX, PropsWithChildren } from 'react';
 import './styles.css';
 
 type SpaceValues = '2xs' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl' | '5xl' | '6xl';
@@ -6,39 +6,15 @@ type SpaceValues = '2xs' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | '4
 interface PanelProps {
   space?: SpaceValues;
   spaceBlock?: SpaceValues;
-  spaceBlockStart?: SpaceValues;
-  spaceBlockEnd?: SpaceValues;
   spaceInline?: SpaceValues;
-  spaceInlineStart?: SpaceValues;
-  spaceInlineEnd?: SpaceValues;
 }
 
 export function Panel(props: PropsWithChildren<PanelProps>): JSX.Element {
-  const spaceInlineStart = props.spaceInlineStart || props.spaceInline || props.space;
-  const spaceInlineEnd = props.spaceInlineEnd || props.spaceInline || props.space;
-  const spaceBlockStart = props.spaceBlockStart || props.spaceBlock || props.space;
-  const spaceBlockEnd = props.spaceBlockEnd || props.spaceBlock || props.space;
-
-  const style: CSSProperties = {};
-
-  if (spaceInlineStart) {
-    style['--ma-space-inline-start'] = `var(--basis-space-inline-${spaceInlineStart})`;
-  }
-
-  if (spaceInlineEnd) {
-    style['--ma-space-inline-end'] = `var(--basis-space-inline-${spaceInlineEnd})`;
-  }
-
-  if (spaceBlockStart) {
-    style['--ma-space-block-start'] = `var(--basis-space-block-${spaceBlockStart})`;
-  }
-
-  if (spaceBlockEnd) {
-    style['--ma-space-block-end'] = `var(--basis-space-block-${spaceBlockEnd})`;
-  }
+  const spaceInline = props.spaceInline ?? props.space;
+  const spaceBlock = props.spaceBlock ?? props.space;
 
   return (
-    <section className="ma-panel" style={style}>
+    <section className="ma-panel" data-space-inline={spaceInline} data-space-block={spaceBlock}>
       {props.children}
     </section>
   );
