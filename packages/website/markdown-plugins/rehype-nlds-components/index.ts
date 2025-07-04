06051281e21d110730e3dfe9faa360dbbd2be0b4
Peter Goes <petergoes@gmail.com>
2025-07-04 14:42:00 +0200

    feat: add emphasis as a markdown component

--- packages/website/markdown-plugins/rehype-nlds-components/index.ts
+++ packages/website/markdown-plugins/rehype-nlds-components/index.ts
@@ -2,6 +2,7 @@ import type { Root } from 'hast';
 import { visit } from 'unist-util-visit';
 import { blockquote } from './blockquote.ts';
 import { code } from './code.ts';
+import { emphasis } from './emphasis.ts';
 
 /**
  * A rehype plugin to transform HTML generated from Markdown so that it uses
@@ -12,6 +13,7 @@ export function nldsComponentsPlugin() {
     visit(tree, 'element', function (node, _index, parent) {
       blockquote(node);
       code(node, parent);
+      emphasis(node);
     });
   };
 }
