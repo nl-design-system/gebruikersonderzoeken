3f62c1659d3e9a2b845a76d69df4394aa5aadb8a
Peter Goes <petergoes@gmail.com>
2025-07-31 16:15:11 +0200

    feat: add local gebruikersonderzoeken-footer component
    A wrapper component specific for the page footer in
    gebruikersonderzoeken.nl

---
+++ packages/website/src/components/gebruikersonderzoeken-footer/gebruikersonderzoeken-footer.tsx
@@ -0,0 +1,80 @@
+import { Heading } from '@nl-design-system-candidate/heading-react';
+import { Paragraph } from '@nl-design-system-candidate/paragraph-react';
+import {
+  PageFooter,
+  PageFooterSection,
+} from '@nl-design-system-community/ma-components/local/page-footer/page-footer.tsx';
+import { IconArrowRight, IconChevronRight } from '@tabler/icons-react';
+import { ButtonLink } from '@utrecht/component-library-react';
+
+export function GebruikersonderzoekenFooter() {
+  return (
+    <PageFooter
+      sections={[
+        <PageFooterSection
+          key="1"
+          action={
+            <ButtonLink href="#" appearance="primary-action-button">
+              Ga naar NL Design System <IconArrowRight />
+            </ButtonLink>
+          }
+        >
+          <Heading level={2} appearance="level-4">
+            Over Gebruikersonderzoeken
+          </Heading>
+          <Paragraph>
+            Gebruikersonderzoeken.nl is een initiatief van het NL Design System. Je vindt hier inzichten uit
+            gebruikersonderzoeken bij de overheid.
+          </Paragraph>
+        </PageFooterSection>,
+
+        <PageFooterSection
+          key="2"
+          action={
+            <ButtonLink href="#" appearance="primary-action-button">
+              Gebruikersonderzoek delen <IconArrowRight />
+            </ButtonLink>
+          }
+        >
+          <Heading level={2} appearance="level-4">
+            Onderzoek delen
+          </Heading>
+          <Paragraph>
+            Heb je zelf onderzoek gedaan dat relevant is voor anderen? Deel je inzichten op een laagdrempelige manier.
+          </Paragraph>
+        </PageFooterSection>,
+
+        <PageFooterSection key="3">
+          <Heading level={2} appearance="level-4">
+            Hulp nodig
+          </Heading>
+          <ul className="utrecht-link-list utrecht-link-list--html-ul">
+            <li className="utrecht-link-list__item">
+              <a className="utrecht-link-list__link" href="/#">
+                <IconChevronRight />
+                Veel gestelde vragen
+              </a>
+            </li>
+            <li className="utrecht-link-list__item">
+              <a className="utrecht-link-list__link" href="/#">
+                <IconChevronRight />
+                Contact
+              </a>
+            </li>
+          </ul>
+        </PageFooterSection>,
+      ]}
+      metaLinks={[
+        <a key="1" className="nl-link" href="/#">
+          Toegankelijkheid
+        </a>,
+        <a key="2" className="nl-link" href="/#">
+          Privacyverklaring
+        </a>,
+        <a key="3" className="nl-link" href="/#">
+          Colofon
+        </a>,
+      ]}
+    />
+  );
+}
