2010264c6c23111915a3b27060956fb69951faf5
Peter Goes <petergoes@gmail.com>
2025-08-01 13:57:25 +0200

    refactor: move search component to packages/website to make it a local component

--- packages/website/src/components/search/search.tsx
+++ packages/website/src/components/search/search.tsx
@@ -1,11 +1,15 @@
+import { IconSearch } from '@tabler/icons-react';
 import './search.css';
+import { Button } from '@utrecht/component-library-react';
 
 export function Search() {
   /* @FIXME: This is only the markup. There is nog functionality behind it yet */
   return (
     <search className="ma-search">
       <input type="text" className="utrecht-textbox" placeholder="Bijvoorbeeld zwembad of grofvuil" />
-      <button className="utrecht-button utrecht-button--secondary-action">Zoeken</button>
+      <Button appearance="secondary-action-button">
+        <IconSearch /> Zoeken
+      </Button>
     </search>
   );
 }
