f8ef45aa4301eae700ae20ec4542bbf44cba01a6
Peter Goes <petergoes@gmail.com>
2025-08-01 09:43:25 +0200

    refactor: rename site-header to page-header

--- packages/ma-components/local/page-header/page-header.tsx
+++ packages/ma-components/local/page-header/page-header.tsx
@@ -1,20 +1,20 @@
 import { Logo } from '../logo/logo';
 import { MaNavigationBar, MaNavigationBarItem, type MaNavigationBarItemProps } from '../navigation-bar/navigation-bar';
-import './site-header.css';
+import './page-header.css';
 import '@utrecht/link-css/dist/index.css';
 import '@utrecht/textbox-css/dist/index.css';
 import '@utrecht/button-css/dist/index.css';
 import { Search } from '../search/search';
 
-export interface SiteHeaderProps {
+export interface PageHeaderProps {
   navigationBarItems?: MaNavigationBarItemProps[];
 }
 
-export function SiteHeader(props: SiteHeaderProps) {
+export function PageHeader(props: PageHeaderProps) {
   return (
-    <header className="ma-site-header">
+    <header className="ma-page-header">
       <section>
-        <div className="ma-site-header__content">
+        <div className="ma-page-header__content">
           <button data-menu-trigger className="utrecht-button">
             Menu
           </button>
@@ -26,7 +26,7 @@ export function SiteHeader(props: SiteHeaderProps) {
         </div>
       </section>
 
-      <section className="ma-site-header__navigation">
+      <section className="ma-page-header__navigation">
         <MaNavigationBar>
           {(props.navigationBarItems || []).map((item) => (
             <MaNavigationBarItem key={item.href} href={item.href} aria-current={item.current || null}>
