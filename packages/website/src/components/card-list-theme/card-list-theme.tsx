9f58fcea9c590e1deced185e4795e00e7141ddd2
Peter Goes <petergoes@gmail.com>
2025-08-29 15:07:54 +0200

    feat: add local card-list-onderzoek and card-list-theme components

---
+++ packages/website/src/components/card-list-theme/card-list-theme.tsx
@@ -0,0 +1,51 @@
+import type { CollectionEntry } from 'astro:content';
+import type { PropsWithChildren } from 'react';
+import { Heading } from '@nl-design-system-candidate/heading-react';
+import { CardAsLinkTheme } from '@nl-design-system-community/ma-components/local/card-as-link/card-as-link.tsx';
+import { CardList } from '@nl-design-system-community/ma-components/local/card-list/card-list.tsx';
+import { dynamicImports } from '@tabler/icons-react';
+
+interface ThemeEntryWithItems extends CollectionEntry<'themes'> {
+  items: object[]
+}
+
+export interface CardListThemeProps {
+  themes: ThemeEntryWithItems[]
+}
+
+interface IconProps {
+  name: string;
+}
+
+async function Icon(props: IconProps) {
+  const dynamicImport = dynamicImports.default[props.name];
+  if (!dynamicImport) return null;
+
+  const { default: DynamicIcon } = await dynamicImport();
+
+  return <DynamicIcon />;
+}
+
+export async function CardListTheme(props: PropsWithChildren<CardListThemeProps>) {
+  return (
+    <CardList>
+      {props.themes.map((theme) => (
+        <CardAsLinkTheme
+          icon={<Icon name={theme.data.icon} />}
+          heading={
+            <Heading level={2} appearance="level-3">
+              {theme.data.title}
+            </Heading>
+          }
+          href={`/docs/thema/${theme.id}`}
+          description={theme.data.description}
+          metadata={[
+            {
+              label: `${theme.items.length} onderzoeken`,
+            },
+          ]}
+        />
+      ))}
+    </CardList>
+  );
+}
