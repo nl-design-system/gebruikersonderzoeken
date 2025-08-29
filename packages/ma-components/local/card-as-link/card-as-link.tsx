ad5519289b7ea7366b4b3aafb97e1c1367205ff3
Peter Goes <petergoes@gmail.com>
2025-08-29 15:07:06 +0200

    feat: add ma-components/card-as-link component

---
+++ packages/ma-components/local/card-as-link/card-as-link.tsx
@@ -0,0 +1,73 @@
+import { ReactNode } from 'react';
+import './card-as-link.css';
+
+interface MetadataProps {
+  icon?: ReactNode;
+  label: string;
+}
+
+export interface CardAsLinkProps {
+  className?: string;
+  heading: ReactNode;
+  href?: string;
+  description?: ReactNode;
+  metadata?: MetadataProps | MetadataProps[];
+  footer?: ReactNode;
+}
+
+export function CardAsLink(props: CardAsLinkProps) {
+  const metadata =
+    Array.isArray(props.metadata) === false ? (
+      <div className="ma-card-as-link__metadata">
+        {props.metadata.icon && <span className="ma-card-as-link__metadata-icon">{props.metadata.icon}</span>}
+        {props.metadata.label}
+      </div>
+    ) : (
+      <ul className="ma-card-as-link__metadata-list">
+        {props.metadata.map((metadata) => (
+          <li key={metadata.label} className="ma-card-as-link__metadata">
+            {metadata.icon && <span className="ma-card-as-link__metadata-icon">{metadata.icon}</span>}
+            {metadata.label}
+          </li>
+        ))}
+      </ul>
+    );
+
+  return (
+    <div className="ma-card-as-link__container">
+      <article className={`ma-card-as-link ${props.className || ''}`.trim()}>
+        <header className="ma-card-as-link__header">
+          {props.href ? (
+            <a className="nl-link" href={props.href}>
+              {props.heading}
+            </a>
+          ) : (
+            props.heading
+          )}
+        </header>
+
+        <p className="ma-card-as-link__description nl-paragraph">{props.description}</p>
+
+        <footer className="ma-card-as-link__footer">{metadata}</footer>
+      </article>
+    </div>
+  );
+}
+
+export interface CardAsLinkThemeProps extends CardAsLinkProps {
+  icon?: ReactNode;
+}
+
+export function CardAsLinkTheme(props: CardAsLinkThemeProps) {
+  const { heading, href, icon, ...restProps } = props;
+  const headingElements = (
+    <>
+      <div className="ma-card-as-link-theme__icon">{icon}</div>
+      <a className="nl-link" href={href}>
+        {heading}
+      </a>
+    </>
+  );
+
+  return <CardAsLink {...restProps} className="ma-card-as-link-theme" heading={headingElements} />;
+}
