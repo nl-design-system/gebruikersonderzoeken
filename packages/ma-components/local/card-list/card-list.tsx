ce8a0d46809dddb484916256ea3f4efd724b0878
Peter Goes <petergoes@gmail.com>
2025-08-29 15:07:29 +0200

    feat: add ma-components/card-list component

---
+++ packages/ma-components/local/card-list/card-list.tsx
@@ -0,0 +1,15 @@
+import './card-list.css';
+
+export function CardList(props: CardListProps) {
+  const children = Array.isArray(props.children) ? props.children : [props.children]
+
+  return (
+    <ul className="ma-card-list">
+      {children.map((child, index) => (
+        <li key={`${index}`}>
+          {child}
+        </li>
+      ))}
+    </ul>
+  );
+}
