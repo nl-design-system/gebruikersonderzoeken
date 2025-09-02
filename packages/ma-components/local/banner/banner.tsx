8c65b65682521331768e2a451cb7ddfeeddf27f6
Peter Goes <petergoes@gmail.com>
2025-09-02 10:39:20 +0200

    feat: add ma-componentes/banner component

---
+++ packages/ma-components/local/banner/banner.tsx
@@ -0,0 +1,37 @@
+import type { ReactNode } from 'react';
+import './banner.css';
+import { BodyCopy } from '../body-copy/body-copy';
+
+export interface BannerProps {
+  version?: 'hero' | 'updates';
+  imageAppearance?: 'small' | 'large';
+  image?: ReactNode;
+  heading?: ReactNode;
+  content?: ReactNode;
+  footer?: ReactNode;
+}
+
+export function Banner(props: BannerProps) {
+  return (
+    <section
+      className={`
+        ma-banner
+        ${props.version === 'hero' ? 'ma-banner--hero' : ''}
+        ${props.version === 'updates' ? 'ma-banner--updates' : ''}
+        ${props.imageAppearance === 'small' ? 'ma-banner--image-appearance-small' : ''}
+      `.trim()}
+    >
+      <div className="ma-banner__content">
+        {props.image && <div className="ma-banner__image">{props.image}</div>}
+
+        {props.heading && <header className="ma-banner__header">{props.heading}</header>}
+
+        <div className="ma-banner__copy">
+          <BodyCopy small>{props.content}</BodyCopy>
+        </div>
+
+        {props.footer && <footer className="ma-banner__footer">{props.footer}</footer>}
+      </div>
+    </section>
+  );
+}
