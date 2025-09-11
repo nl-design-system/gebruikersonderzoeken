b253b50099ac1bb41ff3d9d763a08854c0eec907
Peter Goes <petergoes@gmail.com>
2025-09-11 13:20:36 +0200

    feat: style NavigationDrawer component

--- packages/ma-components/local/navigation-drawer/navigation-drawer.tsx
+++ packages/ma-components/local/navigation-drawer/navigation-drawer.tsx
@@ -1,5 +1,12 @@
 import './navigation-drawer.css';
 
+interface NavigationDrawerItemProps {
+  expandable?: boolean;
+  current?: boolean;
+  label: string;
+  href: string;
+}
+
 function Header() {
   return (
     <header className="ma-navigation-drawer__header">
@@ -10,17 +17,24 @@ function Header() {
   );
 }
 
-function Item() {
-  return <li className="ma-navigation-drawer__item">foo</li>;
+function Item(props: NavigationDrawerItemProps) {
+  return (
+    <li className="ma-navigation-drawer__item ma-focus-indicator-inset">
+      <a href={props.href} aria-current={props.current ? 'page' : null}>
+        {props.label}
+      </a>
+    </li>
+  );
 }
 
 export function NavigationDrawer() {
   return (
     <dialog id="ma-navigation-drawer" className="ma-navigation-drawer">
       <Header />
-      <ul role="list">
-        <Item />
-        <Item />
+      <ul className="ma-navigation-drawer__list" role="list">
+        <Item label="foo" href="#" />
+        <Item label="bar" href="#" current />
+        <Item label="baz" href="#" />
       </ul>
     </dialog>
   );
