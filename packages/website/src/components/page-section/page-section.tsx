ffbdba16eacb5102a4236deb5cc6730b7fc70d5e
Peter Goes <petergoes@gmail.com>
2025-09-30 10:54:15 +0200

    chore: improve page-section

--- packages/website/src/components/page-section/page-section.tsx
+++ packages/website/src/components/page-section/page-section.tsx
@@ -1,6 +1,19 @@
 import type { PropsWithChildren } from 'react';
 import './page-section.css';
 
-export function PageSection(props: PropsWithChildren) {
-  return <section className="ma-page-section">{props.children}</section>;
+export interface PageSectionProps {
+  withSpace?: boolean;
+}
+
+export function PageSection(props: PropsWithChildren<PageSectionProps>) {
+  return (
+    <section
+      className={`
+        ma-page-section
+        ${props.withSpace ? 'ma-page-section--with-space' : ''}
+      `.trim()}
+    >
+      {props.children}
+    </section>
+  );
 }
