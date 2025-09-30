9857eb6c2ea4de4537788c25c6b03e0b4f36ad29
Peter Goes <petergoes@gmail.com>
2025-09-30 10:45:22 +0200

    chore: remove content-wrapper component

--- packages/website/src/components/content-wrapper/content-wrapper.tsx
+++
@@ -1,6 +0,0 @@
-import type { PropsWithChildren } from 'react';
-import './content-wrapper.css';
-
-export function ContentWrapper(props: PropsWithChildren) {
-  return <section className="ma-content-wrapper">{props.children}</section>;
-}
