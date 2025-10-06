8f14448fd359bd57d7a87850080a049613c0fd74
Peter Goes <petergoes@gmail.com>
2025-10-06 16:36:47 +0200

    feat: use nldsComponentsRemarkPlugin in Astro config

--- packages/website/astro.config.ts
+++ packages/website/astro.config.ts
@@ -1,9 +1,10 @@
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-import { nldsComponentsPlugin } from './markdown-plugins/rehype-nlds-components/index.ts';
+import { nldsComponentsPlugin as nldsComponentsRehypePlugin } from './markdown-plugins/rehype-nlds-components/index.ts';
 import { addTrailingSlashPlugin } from './markdown-plugins/rehype-trailing-slash/index.ts';
 import { coverPlugin } from './markdown-plugins/remark-cover/index.ts';
+import { nldsComponentsPlugin as nldsComponentsRemarkPlugin } from './markdown-plugins/remark-nlds-components/index.ts';
 import { removeH1FromMarkdown } from './markdown-plugins/remark-remove-h1/index.ts';
 
 const siteUrl = 'https://gebruikersonderzoeken.nl';
@@ -24,8 +25,8 @@ export default defineConfig({
     }),
   ],
   markdown: {
-    rehypePlugins: [nldsComponentsPlugin, addTrailingSlashPlugin({ siteUrl, stripOrigin: true })],
-    remarkPlugins: [coverPlugin, removeH1FromMarkdown({ filter: 'onderzoek-bekijken' })],
+    rehypePlugins: [nldsComponentsRehypePlugin, addTrailingSlashPlugin({ siteUrl, stripOrigin: true })],
+    remarkPlugins: [nldsComponentsRemarkPlugin, coverPlugin, removeH1FromMarkdown({ filter: 'onderzoek-bekijken' })],
   },
   site: siteUrl,
 });
