f98ea56fa9a99bd339690fa65d5784ca6cde78f8
Peter Goes <petergoes@gmail.com>
2026-01-27 16:21:35 +0100

    chore: move search results in client component

--- packages/website/src/components/search/algolia-api/fetch-results.ts
+++ packages/website/src/components/search/algolia-api/fetch-results.ts
@@ -4,15 +4,15 @@ const appId = 'HWYTAR8XU5';
 const appKey = '74627f8933dc6059f68f48ee8fbecaa9';
 const index = 'gebruikersonderzoek';
 
-export async function fetchResults(searchParams: URLSearchParams) {
+export async function fetchResults(query: string) {
   let result;
 
-  if (searchParams.has('query')) {
+  if (query) {
     result = await fetch(`https://${appId}.algolia.net/1/indexes/${index}/query`, {
       body: JSON.stringify({
         highlightPostTag: '</mark>',
         highlightPreTag: '<mark>',
-        query: searchParams.get('query'),
+        query,
       }),
       headers: {
         'x-algolia-application-id': appId,
--- packages/website/src/components/search/algolia-api/group-hits.ts
+++ packages/website/src/components/search/algolia-api/group-hits.ts
@@ -1,18 +1,38 @@
 import type { Hit } from './types.js';
 
-export function groupHits(hits: Hit[]) {
-  const map = new Map<Hit['url_without_anchor'], Hit['hierarchy']['lvl0']>();
+interface Group {
+  hierarchy: 'lvl0' | 'lvl1' | 'lvl2' | 'lvl3' | 'lvl4' | 'lvl5' | 'lvl6';
+  title: string;
+  value: string;
+  urlWithoutAnchor: string;
+  snippet?: string;
+}
 
-  hits.forEach((hit) => {
-    const snippets = {
-      content: [],
-      lvl0: '',
-    };
-    snippets.lvl0 = hit._highlightResult.hierarchy.lvl0.value;
-    snippets.content = [...snippets.content, hit?._snippetResult?.content?.value].filter((x) => x);
+type Groups = Map<Group['title'], Group>;
 
-    map.set(hit.url_without_anchor, snippets);
+export function groupHits(_hits: Hit[]) {
+  const groups: Groups = new Map();
+  console.log(_hits);
+
+  _hits.forEach((hit) => {
+    const title = hit.hierarchy.lvl0;
+    if (groups.has(title) === false) {
+      const group: Group = {
+        hierarchy: 'lvl0',
+        title,
+        urlWithoutAnchor: hit.url_without_anchor,
+        value: hit?._highlightResult?.hierarchy?.lvl0?.value || hit.hierarchy.lvl0,
+      };
+      groups.set(title, group);
+    }
+
+    if (hit?._snippetResult?.content?.value) {
+      const group = groups.get(title);
+      if (group) {
+        group.snippet = hit._snippetResult.content.value;
+      }
+    }
   });
 
-  return map;
+  return [...groups.values()];
 }
