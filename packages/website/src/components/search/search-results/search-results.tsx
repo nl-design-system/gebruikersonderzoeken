7d2ed9675619cb8fb5ed0a7fb34039cead51db7f
Peter Goes <petergoes@gmail.com>
2026-02-09 15:23:22 +0100

    feat: create a SearchResults component to render search results

---
+++ packages/website/src/components/search/search-results/search-results.tsx
@@ -0,0 +1,19 @@
+import type { SearchResult } from '../types.js';
+
+export interface SearchResultsProps {
+  results: SearchResult[];
+}
+
+export function SearchResults(props: SearchResultsProps) {
+  return (
+    <ul role="list">
+      {props.results.map((result) => (
+        <li key={result.url}>
+          <a href={result.url} dangerouslySetInnerHTML={{ __html: result.title }} />
+          <br />
+          {result.snippets?.map((snippet) => <span key={snippet} dangerouslySetInnerHTML={{ __html: snippet }} />)}
+        </li>
+      ))}
+    </ul>
+  );
+}
