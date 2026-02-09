e65bf1691c36fb47356a4e2c40d2afb6f0c630a2
Peter Goes <petergoes@gmail.com>
2026-02-09 15:24:00 +0100

    feat: render SearchResults or SearchError components in SearchPage

--- packages/website/src/components/search/search-page/search-page.tsx
+++ packages/website/src/components/search/search-page/search-page.tsx
@@ -1,12 +1,15 @@
 import { PageSection } from '@components/page-section/page-section.tsx';
 import { SearchForm } from '@components/search/search-form/search-form.tsx';
 import { Heading } from '@nl-design-system-candidate/heading-react';
+import { Paragraph } from '@nl-design-system-candidate/paragraph-react';
 import { BodyCopy } from '@nl-design-system-community/ma-components/body-copy/body-copy.tsx';
 import { updateDocumentTitle } from '@utils/document-title.ts';
 import { useEffect, useState } from 'react';
 import type { SearchResult } from '../types.js';
-import { fetchResults } from '../algolia-api/fetch-results.ts';
-import { groupHitsToPages } from '../algolia-api/group-hits-to-pages.ts';
+import { fetchResults, SearchError as SearchErrorClass } from '../algolia-api/fetch-results.ts';
+import { groupHitsToPages, type GroupError } from '../algolia-api/group-hits-to-pages.ts';
+import { SearchError } from '../search-error/search-error.tsx';
+import { SearchResults } from '../search-results/search-results.tsx';
 
 function pageTitle(query?: string | null) {
   return query ? `Zoeken naar: "${query}"` : 'Zoeken';
@@ -29,6 +32,7 @@ export const SearchPage = () => {
   const [searchQuery, setSearchQuery] = useState<string | undefined | null>(null);
   const [loading, setLoading] = useState(false);
   const [results, setResults] = useState<SearchResult[]>();
+  const [error, setError] = useState<SearchErrorClass | GroupError | undefined>();
 
   useEffect(() => {
     const url = new URL(document.URL);
@@ -38,15 +42,16 @@ export const SearchPage = () => {
 
   useEffect(() => {
     updateUrlParameter('query', searchQuery);
+    setError(undefined);
+    setResults(undefined);
 
     if (searchQuery) {
       setLoading(true);
       fetchResults(searchQuery)
         .then(groupHitsToPages)
-        .then(([, searchResults]) => {
-          if (searchResults) {
-            setResults(searchResults);
-          }
+        .then(([searchError, searchResults]) => {
+          if (searchError) setError(searchError);
+          if (searchResults) setResults(searchResults);
         })
         .finally(() => setLoading(false));
     }
@@ -66,21 +71,10 @@ export const SearchPage = () => {
 
       <PageSection>
         <BodyCopy small>
-          {loading ? (
-            <p>Aan het zoeken</p>
-          ) : (
-            <ul id="result-list">
-              {results?.map((result) => (
-                <li key={result.url}>
-                  <a href={result.url} dangerouslySetInnerHTML={{ __html: result.title }} />
-                  <br />
-                  {result.snippets?.map((snippet) => (
-                    <span key={snippet} dangerouslySetInnerHTML={{ __html: snippet }} />
-                  ))}
-                </li>
-              ))}
-            </ul>
-          )}
+          {!searchQuery && <Paragraph>Gebruik het zoekveld om te zoeken in de gebruikersonderzoeken</Paragraph>}
+          {loading && <Paragraph>Aan het zoeken</Paragraph>}
+          {results && <SearchResults results={results} />}
+          {error && <SearchError error={error} />}
         </BodyCopy>
       </PageSection>
     </>
