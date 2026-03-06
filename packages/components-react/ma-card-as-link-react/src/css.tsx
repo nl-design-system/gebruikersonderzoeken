7ee902f351014e534d602627d6f9b9ed6fa6c882
Peter Goes <petergoes@gmail.com>
2026-03-06 16:18:52 +0100

    chore: move card-as-link component to publishable package

---
+++ packages/components-react/ma-card-as-link-react/src/css.tsx
@@ -0,0 +1,3 @@
+import '@nl-design-system-community/ma-card-as-link-css/ma-card-as-link.css';
+
+export { CardAsLink } from './ma-card-as-link';
---
+++ packages/website/src/components/card-as-link/card-as-link.tsx
@@ -0,0 +1,8 @@
+import {
+  CardAsLink as MaCardAsLink,
+  CardAsLinkTheme as MaCardAsLinkTheme,
+} from '@nl-design-system-community/ma-card-as-link-react';
+import '@nl-design-system-community/ma-card-as-link-css/ma-card-as-link.css';
+
+export const CardAsLink = MaCardAsLink;
+export const CardAsLinkTheme = MaCardAsLinkTheme;
