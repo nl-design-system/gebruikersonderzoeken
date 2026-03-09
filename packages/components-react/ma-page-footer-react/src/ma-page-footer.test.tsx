79d3866d84dcd81e52f5becfc1e6e5958899fb90
Peter Goes <petergoes@gmail.com>
2026-03-09 13:49:34 +0100

    chore: move page-footer component to publishable package

---
+++ packages/components-react/ma-page-footer-react/src/ma-page-footer.test.tsx
@@ -0,0 +1,26 @@
+import '@testing-library/jest-dom/vitest';
+import { cleanup, render, screen } from '@testing-library/react';
+import { createRef } from 'react';
+import { afterEach, describe, expect, it } from 'vitest';
+import { PageFooter } from './page-footer';
+
+const displayName = 'PageFooter';
+
+afterEach(() => {
+  cleanup();
+});
+
+describe('PageFooter', () => {
+  it(`has displayName "${displayName}"`, () => {
+    expect(PageFooter.displayName).toBe(displayName);
+  });
+
+  it('forwards React refs to the HTMLButtonElement', () => {
+    const ref = createRef<HTMLDivElement>();
+    render(<PageFooter ref={ref}>page-footer</PageFooter>);
+    const element = screen.getByText('page-footer');
+
+    expect(ref.current).toBe(element);
+    expect(element).toBeInstanceOf(HTMLDivElement);
+  });
+});
