75c731c626522d5cf47fc1af07fb825ed89f50f5
Peter Goes <petergoes@gmail.com>
2026-04-02 13:28:27 +0200

    chore: install and use postcss package internally

--- packages/website/postcss.config.ts
+++ packages/website/postcss.config.ts
@@ -1,5 +1,6 @@
-import maDesignTokens from './postcss/ma-design-tokens/plugin.ts';
+import * as designTokens from '@nl-design-system-community/ma-design-tokens/dist/variables.mjs';
+import removeUnusedDesignTokens from '@nl-design-system-unstable/postcss-remove-unused-design-tokens';
 
 export default {
-  plugins: [maDesignTokens()],
+  plugins: [removeUnusedDesignTokens({ designTokens })],
 };
